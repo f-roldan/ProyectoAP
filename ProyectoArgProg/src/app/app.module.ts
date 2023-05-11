6bfe516f73c85698467820e325bd728b86822e0b
f-roldan <fedederly97@gmail.com>
2023-05-10 23:32:40 -0300

    Division De componentes

--- ProyectoArgProg/src/app/app.module.ts
+++ ProyectoArgProg/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { MainComponent } from './componentes/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
